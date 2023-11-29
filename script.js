6b61e0ea7f66cd3a1f984c62a908860b89306e47
cotsdev93 <cotsdev93@gmail.com>
2023-11-29 19:00:11 -0300

    splash + nav responsive

---
+++ script.js
@@ -0,0 +1,35 @@
+let intro = document.querySelector(".intro");
+let logo = document.querySelector(".logoHeader");
+let logoSpan = document.querySelectorAll(".logo");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
+
+    setTimeout(() => {
+      logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 50);
+      });
+    }, 3000);
+
+    setTimeout(()=>{
+        intro.style.top = "-100vh"
+    },3200)
+  });
+});
+
+const menu = document.querySelector(".menuContainer");
+const navMenu = document.querySelector(".navMenu");
+
+menu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+  menu.classList.toggle("opacity")
+});
+
